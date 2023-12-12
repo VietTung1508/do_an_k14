7a522c3c2394ddf81c5591413b677422f5520b3b
Viet Tung <Viettung150803@gmail.com>
2023-12-12 10:19:07 +0700

    commit fix

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "flowbite-react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-lg font-bold">Hello world!</h1>
+      <Button>Cool</Button>
+    </div>
+  );
+}
+
+export default App;
